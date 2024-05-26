67f67c79ef2bae787d233323eaed66954014318b
leejaehun <ldyjka@gmail.com>
2024-05-26 20:57:34 +0900

    Initial Commit

---
+++ src/test/java/rememmung/be_user/BeUserApplicationTests.java
@@ -0,0 +1,13 @@
+package rememmung.be_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
