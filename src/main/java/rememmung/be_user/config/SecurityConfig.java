34c6ed0ec9b6e870e6633650fe83334f8f7d77b7
leejaehun <ldyjka@gmail.com>
2024-06-01 21:32:02 +0900

    feat(master): session, rest, security 기본 설정

--- src/main/java/rememmung/be_user/config/SecurityConfig.java
+++ src/main/java/rememmung/be_user/config/SecurityConfig.java
@@ -13,36 +13,44 @@ import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
-@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig  {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
         http.csrf(AbstractHttpConfigurer::disable)
-                .headers(headersConfigurer -> headersConfigurer.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)) // For H2 DB
-                .formLogin(AbstractHttpConfigurer::disable)
-                .httpBasic(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests((requests) -> requests
-//                        .requestMatchers(antMatcher("/api/admin/**")).hasRole("ADMIN")
-//                        .requestMatchers(antMatcher("/api/user/**")).hasRole("USER")
-//                        .requestMatchers(antMatcher("/h2-console/**")).permitAll()
-//                        .anyRequest().authenticated()
-                                .anyRequest().permitAll()
-                );
-                //.sessionManagement(sessions -> sessions.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-//                .oauth2Login(configure ->
-//                        configure.authorizationEndpoint(config -> config.authorizationRequestRepository(httpCookieOAuth2AuthorizationRequestRepository))
-//                                .userInfoEndpoint(config -> config.userService(customOAuth2UserService))
-//                                .successHandler(oAuth2AuthenticationSuccessHandler)
-//                                .failureHandler(oAuth2AuthenticationFailureHandler)
-//                );
-//                .oauth2Login( configure ->
-//                        configure.userInfoEndpoint(config -> config.userService(customOAuthUserService))
-//                                .successHandler(oAuth2AuthenticationSuccessHandler)
-//                        );
-
+                .authorizeHttpRequests(requests -> requests
+                        .requestMatchers("/api/**").permitAll()
+                )
+                .httpBasic(AbstractHttpConfigurer::disable);
         return http.build();
     }
+//
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//
+//        http.csrf(AbstractHttpConfigurer::disable)
+//                .headers(headersConfigurer -> headersConfigurer.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)) // For H2 DB
+////                .formLogin(AbstractHttpConfigurer::disable)
+////                .httpBasic(AbstractHttpConfigurer::disable)
+//                .authorizeHttpRequests((requests) -> requests
+////                        .requestMatchers(antMatcher("/api/admin/**")).hasRole("ADMIN")
+////                        .requestMatchers(antMatcher("/api/user/**")).hasRole("USER")
+////                        .requestMatchers(antMatcher("/h2-console/**")).permitAll()
+////                        .anyRequest().authenticated()
+//                                .anyRequest().permitAll()
+//                );
+//                //.sessionManagement(sessions -> sessions.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+////                .oauth2Login(configure ->
+////                        configure.authorizationEndpoint(config -> config.authorizationRequestRepository(httpCookieOAuth2AuthorizationRequestRepository))
+////                                .userInfoEndpoint(config -> config.userService(customOAuth2UserService))
+////                                .successHandler(oAuth2AuthenticationSuccessHandler)
+////                                .failureHandler(oAuth2AuthenticationFailureHandler)
+////                );
+////                .oauth2Login( configure ->
+////                        configure.userInfoEndpoint(config -> config.userService(customOAuthUserService))
+////                                .successHandler(oAuth2AuthenticationSuccessHandler)
+////                        );
+//
+//        return http.build();
+//    }
 }
